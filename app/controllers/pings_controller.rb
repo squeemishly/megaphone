ba7bc2e9a26f5bbf3bf796a5a644d66a29debb03
squee <squeemishly@gmail.com>
2018-10-04 10:01:39 -0600

    create manual ping link

---
+++ app/controllers/pings_controller.rb
@@ -0,0 +1,5 @@
+class PingsController < ApplicationController
+  def index
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'initiators#index'
 
   resources :initiators, only: [:index]
+  resources :pings, only: [:index]
 end
