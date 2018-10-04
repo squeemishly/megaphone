4dc89152f1a1e4aadf59c0756c93c31f0123f0c9
squee <squeemishly@gmail.com>
2018-10-04 10:18:00 -0600

    create ping for updated services list

--- app/controllers/pings_controller.rb
+++ app/controllers/pings_controller.rb
@@ -1,5 +1,13 @@
 class PingsController < ApplicationController
   def index
+    @conn = Faraday.new('https://app.fastly.com/admin/changes?interval=60')
+
+    resp = @conn.get do |req|
+      req.headers["Fastly-key"] = ENV["fastly_api_key"]
+    end
+
+    JSON.parse(resp.body, symbolize_names: true)
+
     redirect_to root_path
   end
 end
