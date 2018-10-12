f1fd760007a1297b59bed89319105667f4b91a62
squee <squeemishly@gmail.com>
2018-10-12 15:42:50 -0700

    refactor logic out to appropriate models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,15 @@ class Customer < ApplicationRecord
   has_many :configurations
 
   validates :customer_id, :name, presence: true
+
+  def self.find_customer(customer_id, service_name)
+    customer = Customer.find_by(customer_id: customer_id)
+
+    if !customer
+      customer = Customer.create(
+        customer_id: customer_id,
+        name: service_name
+      )
+    end
+  end
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -2,4 +2,47 @@ class Service < ApplicationRecord
   belongs_to :customer
 
   validates :service_id, :service_name, :active_version, :customer_id, presence: true
+
+  def self.process_service(service)
+    if active_configuration?(service[:active])
+      create_or_update_service(service)
+    end
+  end
+
+  private
+
+  def self.active_configuration?(service)
+    service != nil
+  end
+
+  def self.create_or_update_service(raw_service)
+    saved_service = Service.find_by(service_id: raw_service[:id])
+
+    if saved_service
+      update_service(saved_service, raw_service)
+    else
+      create_service(raw_service)
+    end
+  end
+
+  def self.update_service(saved_service, service)
+    if saved_service.active_version != service[:number]
+      saved_service.update(
+        service_name: service[:name],
+        active_version: service[:number],
+        previous_version: saved_service[:active_version]
+      )
+    end
+  end
+
+  def self.create_service(service)
+    customer = Customer.find_customer(service[:cust_id], service[:name])
+
+    Service.create(
+      service_id: service[:id],
+      service_name: service[:name],
+      active_version: service[:number],
+      customer_id: customer.id
+    )
+  end
 end
