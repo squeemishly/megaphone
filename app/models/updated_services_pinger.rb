091662fc7ad53335604fdfc5579492b5b41e1acf
squee <squeemishly@gmail.com>
2018-10-12 11:10:22 -0700

    save ping response in DB

--- app/models/updated_services_pinger.rb
+++ app/models/updated_services_pinger.rb
@@ -12,6 +12,28 @@ class UpdatedServicesPinger
       req.headers["Fastly-key"] = ENV["fastly_api_key"]
     end
 
-    JSON.parse(resp.body, symbolize_names: true)
+    pretty_resp = JSON.parse(resp.body, symbolize_names: true)
+
+    pretty_resp.each do |service|
+      if service[:active] != nil
+        config = Service.find_by(service_id: service[:id])
+
+        if config
+          if config.active_version != service[:number]
+            config.update(active_version: service[:number], previous_version: config[:active_version])
+          end
+        else
+          customer = Customer.find_by(customer_id: service[:cust_id])
+
+          if customer
+            Service.create(service_id: service[:id], service_name: service[:name], active_version: service[:number], customer_id: customer.id)
+          else
+            customer = Customer.create(customer_id: service[:cust_id], name: service[:customer])
+
+            Service.create(service_id: service[:id], service_name: service[:name], active_version: service[:number], customer_id: customer.id)
+          end
+        end
+      end
+    end
   end
 end
